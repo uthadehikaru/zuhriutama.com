fd4ab8f0a8e97007bdad29b6199dd9880c43ee00
zuhriutama <zuhriutama@gmail.com>
2023-11-23 14:35:29 +0700

    add tailwind to frontend

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 Route::middleware('auth')->group(function() {
     Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
