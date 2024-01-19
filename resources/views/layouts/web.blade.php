1f7e1963cbafc9283a8ab4806720a360f9e5a622
zuhriutama <zuhriutama@gmail.com>
2024-01-19 14:51:04 +0700

    added link to dashboard

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -55,8 +55,15 @@
             <!-- nav - end -->
 
             <!-- buttons - start -->
+            @auth
+            <a href="{{ route('filament.admin.pages.dashboard') }}"
+                class="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block"
+                >Dashboard</a>
+            @else
             <a href="{{ route('filament.admin.auth.login') }}"
-                class="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block">Login/Register</a>
+                class="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block"
+                >Login/Register</a>
+            @endauth
 
             <button type="button"
                 class="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden">
