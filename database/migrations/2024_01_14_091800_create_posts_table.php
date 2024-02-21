61ed35885d07c704ba4ca94963cd3a79508d4e96
zuhriutama <zuhriutama@gmail.com>
2024-02-21 10:56:39 +0700

    fix migration

--- database/migrations/2024_01_14_091800_create_posts_table.php
+++ database/migrations/2024_01_14_091800_create_posts_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -20,7 +21,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->longText('content')->nullable();
             $table->boolean('is_published')->default(true);
-            $table->timestamp('published_at')->currentTimestamp();
+            $table->timestamp('published_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
