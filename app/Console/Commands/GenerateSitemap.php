5fac9e9300d3a4363242263741ec1fa173ed9ae7
zuhriutama <zuhriutama@gmail.com>
2024-05-27 14:21:37 +0700

    added sitemap generator

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Sitemap';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        SitemapGenerator::create('https://zuhriutama.com')->writeToFile(public_path('sitemap.xml'));
+    }
+}
